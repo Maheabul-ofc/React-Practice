5d0be3d502a02b265e85ed13e3ef5e8c1b581852
Mahea Bul <maheabulwork@gmail.com>
2025-04-20 12:33:56 +0530

    Add the project ThemeChanger

---
+++ 09themeSwitcher/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class", //this is the mode to enable for getting data from HTML
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
